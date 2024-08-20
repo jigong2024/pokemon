604fb06a405f0c9d0e90224162f92b682e889448
jigong2024 <choijming21@naver.com>
2024-08-20 15:12:36 +0900

    포켓몬스터 화면에 뿌리기

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import "../pages/Dex.css";
+import styled from "styled-components";
 
-const PokemonList = () => {
-  return <div className="pokemon-list">PokemonList</div>;
+const PokemonList = ({ MOCK_DATA }) => {
+  const PCard = styled.div`
+    display: flex;
+    width: 150px;
+    height: 200px;
+    border: 1px solid black;
+    margin-right: 30px;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+  `;
+
+  return (
+    <div className="pokemon-list">
+      {MOCK_DATA.map((pokemon) => {
+        return (
+          <PCard key={pokemon.id}>
+            <img src={pokemon.img_url} alt={pokemon.korean_name} />
+            <span>{pokemon.id}</span>
+            <span>{pokemon.korean_name}</span>
+            <button>추가</button>
+          </PCard>
+        );
+      })}
+    </div>
+  );
 };
 
 export default PokemonList;
--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Dashboard from "../components/Dashboard";
 import PokemonList from "../components/PokemonList";
 import "./Dex.css";
+import MOCK_DATA from "../mock";
 
 const Dex = () => {
+  //   console.log(MOCK_DATA);
+
   return (
     <div className="dex-page">
       <Dashboard />
-      <PokemonList />
+      <PokemonList MOCK_DATA={MOCK_DATA} />
     </div>
   );
 };
