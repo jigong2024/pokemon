a9567636c7485088033be29f49ec3e070bda2dd1
jigong2024 <choijming21@naver.com>
2024-08-20 17:26:28 +0900

    PokemonCard로 분리

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import "../pages/Dex.css";
+import styled from "styled-components";
 
-const PokemonCard = () => {
-  return <div>PokemonCard</div>;
+const PCard = styled.div`
+  display: flex;
+  width: 130px;
+  height: 200px;
+  border: 1px solid black;
+  border-radius: 10px;
+  margin: 10px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 25px;
+`;
+
+const PokemonCard = ({ pokemon, handleTravel }) => {
+  return (
+    <PCard key={pokemon.id}>
+      <img src={pokemon.img_url} alt={pokemon.korean_name} />
+      <span>No.{pokemon.id}</span>
+      <span>{pokemon.korean_name}</span>
+      <button onClick={() => handleTravel(pokemon)}>같이 여행하기</button>
+    </PCard>
+  );
 };
 
 export default PokemonCard;
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,31 +1,17 @@
 import React from "react";
 import "../pages/Dex.css";
-import styled from "styled-components";
-
-const PCard = styled.div`
-  display: flex;
-  width: 130px;
-  height: 200px;
-  border: 1px solid black;
-  border-radius: 10px;
-  margin: 10px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  font-size: 25px;
-`;
+import PokemonCard from "./PokemonCard";
 
 const PokemonList = ({ MOCK_DATA, handleTravel }) => {
   return (
     <div className="pokemon-list">
       {MOCK_DATA.map((pokemon) => {
         return (
-          <PCard key={pokemon.id}>
-            <img src={pokemon.img_url} alt={pokemon.korean_name} />
-            <span>No.{pokemon.id}</span>
-            <span>{pokemon.korean_name}</span>
-            <button onClick={() => handleTravel(pokemon)}>같이 여행하기</button>
-          </PCard>
+          <PokemonCard
+            key={pokemon.id}
+            pokemon={pokemon}
+            handleTravel={handleTravel}
+          />
         );
       })}
     </div>
