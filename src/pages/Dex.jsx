be22d8eb40c1fc86b2aaeb600e218c484319dd99
jigong2024 <choijming21@naver.com>
2024-08-21 16:04:54 +0900

    alert sweetalert

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -3,6 +3,7 @@ import Dashboard from "../components/Dashboard";
 import PokemonList from "../components/PokemonList";
 import "./Dex.css";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Dex = () => {
   const navigate = useNavigate();
@@ -15,7 +16,20 @@ const Dex = () => {
 
   const handleTravel = (pokemon) => {
     if (travelPokemon.length >= 6) {
-      alert("최대 6마리의 포켓몬만 여행에 데리고갈 수 있습니다!");
+      // alert("최대 6마리의 포켓몬만 여행에 데리고갈 수 있습니다!");
+      Swal.fire({
+        title: "최대 6마리의 포켓몬만 여행에 데리고갈 수 있습니다!",
+        width: 600,
+        padding: "3em",
+        color: "#716add",
+        background: "#fff url(/images/trees.png)",
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("/images/nyan-cat.gif")
+          left top
+          no-repeat
+        `,
+      });
       return;
     }
 
@@ -24,7 +38,12 @@ const Dex = () => {
     );
 
     if (isAlreadyPokemon) {
-      alert("동일한 포켓몬이 존재합니다!");
+      // alert("동일한 포켓몬이 존재합니다!");
+      Swal.fire({
+        icon: "error",
+        title: "동일한 포켓몬이 존재합니다...",
+        text: "다른 아이를 데려가주세요!",
+      });
     } else {
       setTravelPokemon([...travelPokemon, pokemon]);
     }
